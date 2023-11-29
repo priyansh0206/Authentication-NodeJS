486437b19aa2f8277456b19b3c9ebec46025395b
Priyansh Srivastava <priyansh0206@gmail.com>
2023-11-29 21:42:27 +0530

    Setup the express-ejs-layouts, assets folder as static and scss working

--- index.js
+++ index.js
@@ -12,11 +12,21 @@ const db = require('./config/mongoose');
 //variable for using the SAAS
 const sassMiddleWare = require('node-sass-middleware');
 
+//variable for using the express layouts
+const expressLayouts = require('express-ejs-layouts');
+
 // Middleware to parse URL-encoded data
 app.use(express.urlencoded());
 
+//setting up the express-ejs-layouts for enabling onepage view
+app.use(expressLayouts);
+
+//setting up the views engine to ejs and the directory for views folder
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+//setting the up the assets folder to be used as static
+app.use(express.static('assets'));
 
 //setup the SASS middleware for using the SCSS
 app.use(sassMiddleWare({
@@ -27,8 +37,10 @@ app.use(sassMiddleWare({
     prefix: '/css'
 }));
 
+//setting up the routes
 app.use('/', require('./routes'));
 
+//starting the server
 app.listen(port, function(err){
     if(err){
         console.log(err);
